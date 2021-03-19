728a65f5d174e0fae713a6aee6c6ec1942f83c1a
Alena Drachova <al.drachova@gmail.com>
2021-03-19 22:08:48 +0300

    feat: implement function createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let name = '';
+  console.log('members = ' + members);
+  members = members.sort();
+  console.log('members = ' + members);
+  for (let i = 0; i < members.length; i += 1) {
+    if(typeof(members[i]) === 'string') {
+      name += members[i].trimStart().slice(0, 1);
+    }
+  }
+  name = name.toUpperCase().split('').sort().join('');
+  return name;
 };
