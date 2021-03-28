5babf5804ef3e0b7b2f4e78582adf255f2f820e9
Alena Drachova <al.drachova@gmail.com>
2021-03-28 23:56:03 +0300

    refactor: delete console.log()

--- src/dream-team.js
+++ src/dream-team.js
@@ -5,9 +5,7 @@ module.exports = function createDreamTeam(members) {
     return false;
   }
   let name = '';
-  console.log('members = ' + members);
   members = members.sort();
-  console.log('members = ' + members);
   for (let i = 0; i < members.length; i += 1) {
     if(typeof(members[i]) === 'string') {
       name += members[i].trimStart().slice(0, 1);
