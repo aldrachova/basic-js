4fa9c0c592ec40e3bf0fd5ceab432b248190d066
Alena Drachova <al.drachova@gmail.com>
2021-03-29 20:20:54 +0300

    feat: implement VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,99 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(directMode = true) {
+    this.directMode = directMode;
+  }
+
+  prepareKey(message, key) {
+    let preparedKey = [];
+    let currentKeyIndex = 0;
+    const letters = /[A-Za-z]/;
+    for (let i = 0; i < message.length; i++) {
+      if(currentKeyIndex === key.length){
+        currentKeyIndex = 0;
+      }
+      if (letters.test(message[i])) {
+        preparedKey.push(key[currentKeyIndex]);
+        currentKeyIndex += 1;
+      } else {
+        preparedKey.push(message[i]);
+      }  
+    }
+    return preparedKey.join('');
+  }
+
+  convertToNumbers (str) {
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let strUpperCase = str.toUpperCase();
+    let converted = [];
+    for (let i = 0; i < strUpperCase.length; i++) {
+      if(alphabet.includes(strUpperCase[i])) {
+        converted.push(alphabet.indexOf(strUpperCase[i]));
+      } else {
+        converted.push(strUpperCase.charCodeAt(i));
+      } 
+    }
+    return converted;
+  }
+
+  convertToLetters (arr) {
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let converted = [];
+    for (let i = 0; i < arr.length; i++) {
+      if(arr[i] < 26){
+        converted.push(alphabet[arr[i]]);
+      } else {
+        converted.push(String.fromCharCode(arr[i]));
+      } 
+    }
+    return converted;
+  }
+
+  encrypt(message, key) {
+
+    if(!message || !key) {
+      throw new Error('At least one of parameters has not been given');
+    }
+    let preparedKey = this.prepareKey(message, key);
+    let convertedMessage = this.convertToNumbers(message);
+    let convertedKey = this.convertToNumbers(preparedKey);
+    let result = [];
+    for (let i = 0; i < convertedMessage.length; i++){
+      if(convertedMessage[i] < 26) {
+        result.push((Number.parseInt(convertedMessage[i]) + Number.parseInt(convertedKey[i])) % 26);
+        continue;
+      } else {
+        result.push(convertedMessage[i]);
+      }  
+    }
+    result = this.convertToLetters(result);
+    return this.directMode ? result.join('') : result.reverse().join('');
+  }  
+
+  decrypt(message, key) {
+    if(!message || !key) {
+      throw new Error('At least one of parameters has not been given');
+    }
+    let preparedKey = this.prepareKey(message, key);
+    let convertedMessage = this.convertToNumbers(message);
+    let convertedKey = this.convertToNumbers(preparedKey);
+    let result = [];
+    for (let i = 0; i < convertedMessage.length; i++){
+      if (convertedMessage[i] < 26) {
+        if(convertedMessage[i] < convertedKey[i]){
+          result.push(convertedMessage[i] + 26 - convertedKey[i]);
+        } else {
+          result.push(convertedMessage[i] - convertedKey[i]);
+        } 
+      } else {
+        result.push(convertedMessage[i]);
+      }
+    }
+    result = this.convertToLetters(result);
+
+    return this.directMode ? result.join('') : result.reverse().join('');
   }
 }
 
